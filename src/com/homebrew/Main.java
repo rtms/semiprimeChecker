1b729a4827edc0f6aa6637689c5b9abfc73b3f1e
rtms <redly.tamas@gmail.com>
2020-03-01 23:37:11 +0100

    semiprime checker with CLI

---
+++ src/com/homebrew/Main.java
@@ -0,0 +1,67 @@
+package com.homebrew;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Enter a whole number as a parameter!");
+            return;
+        }
+
+        int number;
+        try {
+            number = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Input is not a number.\nEnter a whole number as a parameter!");
+            return;
+        }
+        System.out.println(number + " is " + (semiPrime(number) ? "" : "not ") + "a semiprime.");
+    }
+
+    static boolean semiPrime(int number) {
+        if (number == 1) {
+            return true;
+        }
+        if (number < 1) {
+            return false;
+        }
+
+        List<Integer> divisors = new ArrayList<>();
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                divisors.add(i);
+            }
+        }
+
+        List<Integer> primeDivisors = divisors.stream()
+                .filter(n -> isPrime(n))
+                .collect(Collectors.toList());
+
+        if (primeDivisors.isEmpty()) {
+            return false;
+        }
+
+        for (int primeDivisor : primeDivisors) {
+            if (primeDivisors.contains(number / primeDivisor)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean isPrime(int number) {
+        if (number == 1) {
+            return true;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
